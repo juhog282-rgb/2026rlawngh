93f123f1a4703abc6da71ad8cefccbc0e10b42e7
juhog282 <juhog282@gmail.com>
2026-03-31 09:52:09 +0900

    charar coin

---
+++ Assets/Settings/player.cs
@@ -0,0 +1,50 @@
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class player : MonoBehaviour
+{
+    public float moveSpeed = 5.0f;
+    public float jumpForce = 5.0f;
+
+    public Rigidbody rb;
+
+    public bool isGrounded = true;
+
+    public bool coinCount = 0;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("HorizontalWrapMode");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        rb.linearVelocity = new Vector3 (moveHorizontal * moveSpeed, rb.linearVelocity.y,moveVertical * moveSpeed);
+        if (Input.GetButtonDown("Jump") && !isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false; 
+        }
+    }
+}
+private void OnCollisonEnter(Collider collision)
+{
+    if (collision.gameObject.tag == "Ground")
+    {
+        isGround = true;
+    }
+}
+
+private void OnTriggerEnter(Collider other)
+
+    if (other.CompareTag("Coin"))
+{
+    coinCount++;
+    OnDestroy(other.gameObject);
+}
